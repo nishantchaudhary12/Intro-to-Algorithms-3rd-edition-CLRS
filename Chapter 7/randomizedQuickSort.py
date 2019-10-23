f490d5003d75f260d22660c2426a19346995467e
Nishant Chaudhary <34044056+nishantchaudhary12@users.noreply.github.com>
2019-10-23 01:32:11 -0500

    CLRS

---
+++ Chapter 7/randomizedQuickSort.py
@@ -0,0 +1,40 @@
+#quick sort
+#randomized partition
+
+import random
+
+def randomized_partition(arr, start, end):
+    i = random.randint(start, end)
+    arr[i], arr[end] = arr[end], arr[i]
+    return partition(arr, start, end)
+
+
+def quickSort(arr, start, end):
+    if start < end:
+        key = randomized_partition(arr, start, end)
+        quickSort(arr, start, key - 1)
+        quickSort(arr, key + 1, end)
+
+
+def partition(arr, start, end):
+    x = arr[end]
+    i = start - 1
+    for j in range(start, end):
+        if arr[j] <= x:
+            i += 1
+            temp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = temp
+    arr[end] = arr[i + 1]
+    arr[i + 1] = x
+    return i + 1
+
+
+def main():
+    arr = [2, 8, 7, 1, 3, 5, 6]
+    print(arr)
+    quickSort(arr, 0, len(arr) - 1)
+    print('Sorted array:', arr)
+
+
+main()
